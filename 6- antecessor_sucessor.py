0a7878562ce37098744b54daaaa739afb5a33068
Camila Bastos Silva <bastoscamilla612@gmail.com>
2026-08-21 16:46:55 -0300

    Add files via upload

---
+++ 6- antecessor_sucessor.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls')
+
+print("= SOLICITANDO DADOS =")
+numero = int(input("Digite um numero: "))
+
+sucessor = numero + 1
+antecessor = numero - 1
+
+print("\n= EXIBINDO DADOS =")
+print("Antecessor: ", antecessor)
+print("Seu número: ", numero)
+print("Sucessor: ", sucessor)
